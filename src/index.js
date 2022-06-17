bf88366cba1ec71804e27955cdc0455cbef7cf06
Lennart <lkarow@online.de>
2022-06-17 09:43:55 +0200

    install atatus

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+import * as atatus from 'atatus-spa';
+
+atatus.config('ad111dd8546842279652b47f7dfb6f94').install();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
