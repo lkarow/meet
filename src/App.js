b79cbe7ba64a85c91b907bf90576bbf4a5d79788
Lennart <lkarow@online.de>
2022-06-20 23:46:09 +0200

    add title

--- src/App.js
+++ src/App.js
@@ -45,6 +45,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <h1>Meet App</h1>
         <CitySerach
           locations={this.state.locations}
           updateEvents={this.updateEvents}
