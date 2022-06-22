3fbd9daa429e8d9f3f1d7251d6776aaea92109cf
Lennart <lkarow@online.de>
2022-06-22 21:05:44 +0200

    fix offline alert

--- src/App.js
+++ src/App.js
@@ -58,7 +58,7 @@ class App extends Component {
     return (
       <div className="App">
         <h1>Meet App</h1>
-        <OfflineAlert />
+        <OfflineAlert text={this.state.offlineText} />
         <CitySerach
           locations={this.state.locations}
           updateEvents={this.updateEvents}
