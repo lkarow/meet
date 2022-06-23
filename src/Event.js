534d79dc877c84c2810d9c7a89df63926bb24693
Lennart <lkarow@online.de>
2022-06-24 00:14:21 +0200

    refactor

--- src/Event.js
+++ src/Event.js
@@ -29,7 +29,7 @@ class Event extends Component {
 
     return (
       <div className="event">
-        <h3 className="title">{event.summary}</h3>
+        <h2 className="title">{event.summary}</h2>
         <p className="start-time">{this.dateNewFormat(event.start.dateTime)}</p>
         <p className="location">{event.location}</p>
         {!this.state.collapsed && (
